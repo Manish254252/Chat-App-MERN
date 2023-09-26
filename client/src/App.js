bacce60edbbedc31333e4b000c6ba08e5fe22901
Manish Solanki <70832573+Manish254252@users.noreply.github.com>
2023-09-26 10:09:40 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Chat from './components/chat';
 
 
 
-const socket = io("https://chat-app-node-kf4x.onrender.com/");
+const socket = io("https://chat-app-node-kf4x.onrender.com");
 
 export default function App() {
 
